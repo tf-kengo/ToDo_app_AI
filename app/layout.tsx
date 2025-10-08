5c5a6e8ab6636646deaac452cf9ddf130279c8c0
k.tanaka <k.tanaka.p3@p1.techfirm.co.jp>
2025-10-08 15:42:15 +0900

    fix: todoリストのcssの調整

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>
-        <main className="container mx-auto py-8">
+        <main>
           {children}
           {modal}
         </main>
